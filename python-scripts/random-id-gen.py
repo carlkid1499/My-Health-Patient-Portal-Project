9e883fcaea6f3c84f9b91bf8939b5bd4e2411b39
Carlos <carlos.santos112@outlook.com>
2020-09-03 12:49:37 -0700

    added random seed, and zero padding

--- python-scripts/random-id-gen.py
+++ python-scripts/random-id-gen.py
@@ -4,6 +4,9 @@
 Title: random-id-gen.py
 Description: This script will generate random identification numbers of length 20.
 Usage: To be used in creating new patient identification numbers.
+import documentation: 
+   https://docs.python.org/3/library/random.html#module-random
+   https://docs.python.org/3/library/argparse.html?highlight=argparse#module-argparse
 '''
 
 # imports
@@ -15,7 +18,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="random-id-gen.py")
     parser.add_argument("-a", type=int, required=True, dest="amount", help="number of ID's to generate")
     args = parser.parse_args()
+    random.seed()
 
     # Print ID's
     for i in range (0, args.amount):
-        print(random.randint(0,1e20))
+        # Must be a string to Zero Pad
+        ran_ID = str(random.randint(0,1e20))
+        # Print and Zero Pad if needed.
+        print(ran_ID.zfill(20))
