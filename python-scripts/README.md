5cc776b9f70ea89bd5efc557e31bcaf939d158b4
Carlos <carlos.santos112@outlook.com>
2020-09-17 15:10:13 -0700

    README updates, added address and phone numbers.

--- python-scripts/README.md
+++ python-scripts/README.md
@@ -3,3 +3,23 @@
 This is a place for all python scripts developed for this project. 
 It will mostly house data generation scripts or other python utilties we
 as a team come up with. Documentation will be added as needed.
+
+## Generator.py
+
+The sole purpose of this script is to generate random data geared towards our project.
+This script can easily be modifed to adapt to changing needs.
+
+###  Command Line Parameters
+
+-a amount of records to generate
+-id flag to set if random IDs are needed. The IDs are 20 digits in length and stored as a string
+-b flag to generate birthdays in the format of: %d-%m-%y
+-n flag to generate names.
+-p flag to generate phone numbers
+-addr flag to generate addresses
+-host host address for a MySQL instance
+-user user name for MySQL instance
+-db database for MySQL instance
+
+### Usage
+
--- python-scripts/generator.py
+++ python-scripts/generator.py
@@ -15,6 +15,8 @@ import random
 import argparse
 from faker import Faker
 from faker.providers.date_time import date
+from faker.providers import phone_number
+from faker.providers import address
 
 # main like in C/C++
 if __name__ == "__main__":
@@ -27,30 +29,44 @@ if __name__ == "__main__":
                         dest="birthday", help="Set if you need birthdays generated")
     parser.add_argument("-n", default=False, action="store_true",
                         dest="name", help="Set if you need names generated")
-    parser.add_argument("-host", type=str, required=True,
+    parser.add_argument("-p", default=False, action="store_true",
+                        dest="phone", help="Set if you need phone numbers generated")
+    parser.add_argument("-addr", default=False, action="store_true",
+                        dest="address", help="Set if you need addresses generated")
+    parser.add_argument("-host", type=str, required=False, default=None,
                         dest="host", help="Hostname for DB")
-    parser.add_argument("-user", type=str, required=True,
+    parser.add_argument("-user", type=str, required=False, default=None,
                         dest="user", help="Username for DB")
-    parser.add_argument("-pass", type=str, required=True,
+    parser.add_argument("-pass", type=str, required=False, default=None,
                         dest="password", help="Password for DB")
-    parser.add_argument("-db", type=str, required=True,
+    parser.add_argument("-db", type=str, required=False, default=None,
                         dest="database", help="Database to connect too")
     args = parser.parse_args()
 
-    # Connect to our MySQL server
-    mydb = mysql.connect(host=args.host, user=args.user, password=args.password, database=args.database)
-    mycursor =mydb.cursor()
-    # Create the able if it alrady doesn't exist
-    mycursor.execute("""CREATE TABLE IF NOT EXISTS patInfo(id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, pID VARCHAR(255) UNIQUE, name VARCHAR(255), phone VARCHAR(255), DOB VARCHAR(255))""")
-
+    if args.host:
+        # Connect to our MySQL server
+        mydb = mysql.connect(host=args.host, user=args.user,
+                             password=args.password, database=args.database)
+        mycursor = mydb.cursor()
+        # Create the able if it alrady doesn't exist
+        mycursor.execute(
+            """CREATE TABLE IF NOT EXISTS patInfo(
+                id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, 
+                pID VARCHAR(255) UNIQUE, 
+                name VARCHAR(255),
+                phone VARCHAR(255),
+                DOB VARCHAR(255)),
+                address VARCHAR(255)""")
 
     random.seed()
     fake = Faker()
-    
+
     for i in range(0, args.amount):
         ran_ID = ""
         ran_DOB = ""
         ran_NAME = ""
+        ran_PHONE = ""
+        ran_ADDRESS = ""
         if args.id:
             # Print IDs
             # Must be a string to Zero Pad
@@ -69,8 +85,22 @@ if __name__ == "__main__":
             ran_NAME = fake.name()
             print(ran_NAME)
 
-        # Insert data into table
-        mycursor.execute("INSERT INTO patInfo(pID, name, DOB) VALUES (%s, %s, %s)", (ran_ID, ran_DOB, ran_NAME))
-        mydb.commit()
+        if args.host:
+            # Insert data into table
+            mycursor.execute("""INSERT INTO patInfo(pID, name, phone, DOB, address) 
+                                VALUES (%s, %s, %s, %s, %s)""",
+                             (ran_ID, ran_NAME, ran_PHONE, ran_DOB, ran_ADDRESS))
+            mydb.commit()
 
-    mydb.close()
+        if args.phone:
+            # Print Phone Numbers
+            ran_PHONE = fake.phone_number()
+            print(ran_PHONE)
+
+        if args.address:
+            # Print Address
+            ran_ADDRESS = fake.address()
+            print(ran_ADDRESS)
+
+    if args.host:
+        mydb.close()
