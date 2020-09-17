2914da6948ac6c6a17b409a7789b676f2de25f32
Carlos <carlos.santos112@outlook.com>
2020-09-17 14:30:33 -0700

    good stopping point. Can create basic table, and insert data into it.

--- python-scripts/generator.py
+++ python-scripts/generator.py
@@ -40,27 +40,37 @@ if __name__ == "__main__":
     # Connect to our MySQL server
     mydb = mysql.connect(host=args.host, user=args.user, password=args.password, database=args.database)
     mycursor =mydb.cursor()
-    # Command to execute
-    mycursor.execute("""SHOW TABLES""")
+    # Create the able if it alrady doesn't exist
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS patInfo(id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, pID VARCHAR(255) UNIQUE, name VARCHAR(255), phone VARCHAR(255), DOB VARCHAR(255))""")
 
 
     random.seed()
     fake = Faker()
-    if args.id:
-        # Print IDs
-        for i in range(0, args.amount):
+    
+    for i in range(0, args.amount):
+        ran_ID = ""
+        ran_DOB = ""
+        ran_NAME = ""
+        if args.id:
+            # Print IDs
             # Must be a string to Zero Pad
             ran_ID = str(random.randint(0, 1e20))
             # Print and Zero Pad if needed.
-            print(ran_ID.zfill(20))
+            ran_ID = ran_ID.zfill(20)
+            print(ran_ID)
 
-    if args.birthday:
-        # Print Birthdays
-        
-        for i in range(0, args.amount):
-            print(fake.date(pattern='%d-%m-%Y', end_datetime=None))
+        if args.birthday:
+            # Print Birthdays
+            ran_DOB = fake.date(pattern='%d-%m-%Y', end_datetime=None)
+            print(ran_DOB)
 
-    if args.name:
-        # Print Names
-        for i in range(0, args.amount):
-            print(fake.name())
+        if args.name:
+            # Print Names
+            ran_NAME = fake.name()
+            print(ran_NAME)
+
+        # Insert data into table
+        mycursor.execute("INSERT INTO patInfo(pID, name, DOB) VALUES (%s, %s, %s)", (ran_ID, ran_DOB, ran_NAME))
+        mydb.commit()
+
+    mydb.close()
