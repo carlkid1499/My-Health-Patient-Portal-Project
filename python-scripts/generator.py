524eb29888c8dc3f4399c68fad87dd87125c7f57
Carlos <carlos.santos112@outlook.com>
2020-09-22 15:11:44 -0700

    adjusted script to align with schemas from assignment 1.

--- python-scripts/generator.py
+++ python-scripts/generator.py
@@ -14,9 +14,6 @@ import mysql.connector as mysql
 import random
 import argparse
 from faker import Faker
-from faker.providers.date_time import date
-from faker.providers import phone_number
-from faker.providers import address
 
 # main like in C/C++
 if __name__ == "__main__":
@@ -32,7 +29,11 @@ if __name__ == "__main__":
     parser.add_argument("-p", default=False, action="store_true",
                         dest="phone", help="Set if you need phone numbers generated")
     parser.add_argument("-c", default=False, action="store_true",
-                        dest="company_name", help="Set if you need company names generated")                    
+                        dest="company_name", help="Set if you need company names generated")
+    parser.add_argument("-e", default=False, action="store_true",
+                        dest="email", help="Set if you need emails generated")
+    parser.add_argument("-patients", default=False, action="store_true",
+                        dest="patients", help="Set if you need to insert into Patients table")
     parser.add_argument("-addr", default=False, action="store_true",
                         dest="address", help="Set if you need addresses generated")
     parser.add_argument("-host", type=str, required=False, default=None,
@@ -46,35 +47,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.host:
-        # Connect to our MySQL server
+        # Connect to our MySQL server.
         mydb = mysql.connect(host=args.host, user=args.user,
                              password=args.password, database=args.database)
         mycursor = mydb.cursor()
-        # Create the able if it alrady doesn't exist
-        mycursor.execute(
-            """CREATE TABLE IF NOT EXISTS patInfo(
-                id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, 
-                pID VARCHAR(255) UNIQUE, 
-                name VARCHAR(255),
-                phone VARCHAR(255),
-                DOB VARCHAR(255)),
-                address VARCHAR(255)""")
+
+    # SQL Statements for tables
+    insert_patients = "INSERT INTO patients(PID, Name, Age, DOB, Address, PEmail, TelNo, EContactName, EContactPhone) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
 
     random.seed()
     fake = Faker()
 
     for i in range(0, args.amount):
+        # Init variables to insert
         ran_ID = ""
         ran_DOB = ""
         ran_NAME = ""
+        ran_ENAME = ""
         ran_PHONE = ""
+        ran_EPHONE = ""
         ran_ADDRESS = ""
         ran_COMPANY_NAME = ""
+        ran_EMAIL = ""
+        ran_AGE = 0
 
         if args.id:
             # Print IDs
             # Must be a string to Zero Pad
-            ran_ID = str(random.randint(0, 1e20))
+            ran_ID = str(random.randint(0, 1e19))
             # Print and Zero Pad if needed.
             ran_ID = ran_ID.zfill(20)
             print(ran_ID)
@@ -87,18 +87,13 @@ if __name__ == "__main__":
         if args.name:
             # Print Names
             ran_NAME = fake.name()
+            ran_ENAME = fake.name()
             print(ran_NAME)
-
-        if args.host:
-            # Insert data into table
-            mycursor.execute("""INSERT INTO patInfo(pID, name, phone, DOB, address) 
-                                VALUES (%s, %s, %s, %s, %s)""",
-                             (ran_ID, ran_NAME, ran_PHONE, ran_DOB, ran_ADDRESS))
-            mydb.commit()
 
         if args.phone:
             # Print Phone Numbers
             ran_PHONE = fake.phone_number()
+            ran_EPHONE = fake.phone_number()
             print(ran_PHONE)
 
         if args.address:
@@ -111,5 +106,24 @@ if __name__ == "__main__":
             ran_COMPANY_NAME = fake.company()
             print(ran_COMPANY_NAME)
 
+        if args.email:
+            # Print Emails
+            ran_EMAIL = fake.email()
+            print(ran_EMAIL)
+
+        if args.host and args.patients:
+            # Insert data into Patients
+            mycursor.execute(insert_patients,
+                             (int(ran_ID),
+                             ran_NAME,
+                             ran_AGE,
+                             ran_DOB,
+                             ran_ADDRESS,
+                             ran_EMAIL,
+                             ran_PHONE,
+                             ran_ENAME,
+                             ran_EPHONE))
+            
     if args.host:
+        mydb.commit()
         mydb.close()
