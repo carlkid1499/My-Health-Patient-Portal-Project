16ebcdd4fcdfeeb6b2059d463465a287cb2f63df
carlkid1499 <carlos.santos112@outlook.com>
2020-10-22 16:06:49 -0700

    added the ability to insert into the Insurance Providers table

--- python-scripts/generator.py
+++ python-scripts/generator.py
@@ -25,7 +25,9 @@ if __name__ == "__main__":
     parser.add_argument("-v", default=False, action="store_true", 
                         dest="verbose", help="verbose output flag")
     parser.add_argument("-patients", default=False, action="store_true",
-                        dest="patients", help="Set if you need to insert into Patients table")
+                        dest="patients", help="Set if you need to insert into Insurance Provider table")
+    parser.add_argument("-provider", default=False, action="store_true",
+                        dest="provider", help="Set if you need to insert into Patients table")
     parser.add_argument("-host", type=str, required=False, default=None,
                         dest="host", help="Hostname for DB")
     parser.add_argument("-port", type=int, required=False, default=None,
@@ -46,6 +48,7 @@ if __name__ == "__main__":
 
     # SQL Statements for tables
     insert_patients = "INSERT INTO PatientInfo(PID, name_first, name_last, DOB, gender, address, email, phone, Emergency_name, Emergency_phone) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    insert_insprovider = "INSERT INTO InsProvider(CompanyID, Company, PlanID, Category, Address, Email, Phone) VALUES (%s, %s, %s, %s, %s, %s, %s)"
 
     # Start the Faker instance
     random.seed()
@@ -54,63 +57,100 @@ if __name__ == "__main__":
     for i in range(0, args.amount):
         try:
 
-            # Let's generate all the data first
-            # Must be a string to Zero Pad, 9223372036854775807 is bigint unsided MySQL max value
-            ran_ID = str(random.randint(0, 9223372036854775807))
-            # Print and Zero Pad if needed.
-            ran_ID = ran_ID.zfill(20)
-            # Birthdays. Lets make them in MySQL format
-            ran_DOB = fake.date(pattern='%Y-%m-%d', end_datetime=None)
-            # Calculate the Age
-            today = date.datetime.today()
-            age = relativedelta(today, date.datetime.strptime(ran_DOB, "%Y-%m-%d"))
-            ran_AGE = age.years
-            if i % 2 == 0:
-                ran_FNAME = fake.first_name_male()
-                ran_LNAME = fake.last_name_male()
-                ran_GENDER = "male"
-            else:
-                ran_FNAME = fake.first_name_female()
-                ran_LNAME = fake.last_name_female()
-                ran_GENDER = "female"
-            
-            ran_ENAME = fake.name()
-            ran_PHONE = fake.phone_number()
-            ran_EPHONE = fake.phone_number()
-            ran_ADDRESS = fake.address()
-            ran_COMPANY_NAME = fake.company()
-            ran_EMAIL = fake.email()
-            
+            # Insert data into Patients
+            if args.patients:
 
-            if args.verbose:
-                print(ran_ID)
-                print(ran_DOB)
-                print(ran_AGE)
-                print(ran_FNAME)
-                print(ran_LNAME)
-                print(ran_GENDER)
-                print(ran_PHONE)
-                print(ran_ADDRESS)
-                print(ran_COMPANY_NAME)
-                print(ran_EMAIL)
+                # Let's generate all the data first
+                # Must be a string to Zero Pad, 9223372036854775807 is bigint unsided MySQL max value
+                ran_ID = str(random.randint(0, 9223372036854775807))
+                # Print and Zero Pad if needed.
+                ran_ID = ran_ID.zfill(20)
+                # Birthdays. Lets make them in MySQL format
+                ran_DOB = fake.date(pattern='%Y-%m-%d', end_datetime=None)
+                # Calculate the Age
+                today = date.datetime.today()
+                age = relativedelta(today, date.datetime.strptime(ran_DOB, "%Y-%m-%d"))
+                ran_AGE = age.years
+                if i % 2 == 0:
+                    ran_FNAME = fake.first_name_male()
+                    ran_LNAME = fake.last_name_male()
+                    ran_GENDER = "male"
+                else:
+                    ran_FNAME = fake.first_name_female()
+                    ran_LNAME = fake.last_name_female()
+                    ran_GENDER = "female"
+                
+                ran_ENAME = fake.name()
+                ran_PHONE = fake.phone_number()
+                ran_EPHONE = fake.phone_number()
+                ran_ADDRESS = fake.address()
+                ran_EMAIL = fake.email()
+                
+                if args.verbose:
+                    print(ran_ID)
+                    print(ran_DOB)
+                    print(ran_AGE)
+                    print(ran_FNAME)
+                    print(ran_LNAME)
+                    print(ran_GENDER)
+                    print(ran_PHONE)
+                    print(ran_ADDRESS)
+                    print(ran_EMAIL)
 
-             # Insert data into Patients
-            if args.host and args.user and args.password and args.database and args.patients:
-                mycursor.execute(insert_patients,
-                                (int(ran_ID),
-                                ran_FNAME,
-                                ran_LNAME,
-                                ran_DOB,
-                                ran_GENDER,
-                                ran_ADDRESS,
-                                ran_EMAIL,
-                                ran_PHONE,
-                                ran_ENAME,
-                                ran_EPHONE))
-                mydb.commit()
-            else:
-                print("Error Inserting into PatientInfo: One of hostname, username, password, database name, or patients flag is missing!")
-                break
+                # Check to make sure the connection stuff is present
+                if args.host and args.port and args.user and args.password and args.database:
+                    mycursor.execute(insert_patients,
+                                        (int(ran_ID),
+                                        ran_FNAME,
+                                        ran_LNAME,
+                                        ran_DOB,
+                                        ran_GENDER,
+                                        ran_ADDRESS,
+                                        ran_EMAIL,
+                                        ran_PHONE,
+                                        ran_ENAME,
+                                        ran_EPHONE))
+                    mydb.commit()
+                
+                else:
+                    print("Error Inserting into PatientInfo: One of hostname, username, password, database name, or patients flag is missing!")
+                    break
+            
+            # Insert into the Insurance Provider table
+            elif args.provider:
+
+                # Generate the Data first
+                ran_CompanyID = random.randint(1, 4294967295)
+                ran_PlanID = random.randint(1, 4294967295)
+                ran_CompanyPhone = fake.phone_number()
+                ran_CompanyAddress = fake.address()
+                ran_CompanyName = fake.company()
+                ran_CompanyEmail = fake.company_email()
+                # This is a list of different types of health insurance https://www.webmd.com/health-insurance/types-of-health-insurance-plans#1
+                cat_list = ['HMO', 'PPO', 'EPO', 'POS', 'HDHP', 'HSA']
+                ran_Category = fake.word(ext_word_list=cat_list)
+
+                if args.verbose:
+                    print(ran_CompanyID)
+                    print(ran_CompanyName)
+                    print(ran_PlanID)
+                    print(ran_Category)
+                    print(ran_CompanyAddress)
+                    print(ran_CompanyEmail)
+                    print(ran_CompanyPhone)
+                    
+                # Check to make sure the connection stuff is present
+                if args.host and args.port and args.user and args.password and args.database:
+                    "INSERT INTO InsProvider(CompanyID, Company, PlanID, Category, Address, Email, Phone) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    mycursor.execute(insert_insprovider,
+                                        (ran_CompanyID,
+                                        ran_CompanyName,
+                                        ran_PlanID,
+                                        ran_Category,
+                                        ran_CompanyAddress,
+                                        ran_CompanyEmail,
+                                        ran_CompanyPhone))
+                    mydb.commit()
 
         except KeyboardInterrupt:
             mydb.commit()
