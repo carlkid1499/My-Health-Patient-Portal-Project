8a3239e1a6f2a6cfab2a003c6ea060abf879a83a
Carlos <carlos.santos112@outlook.com>
2020-10-21 17:00:00 -0700

    Patient Portal
    adding patient portal file

---
+++ php/patient_portal.php
@@ -0,0 +1,30 @@
+<?php
+/* This is the patient_portal.php file.
+* All Patient information will be accessed
+* through this page.
+*/
+
+# import another php file and access it's variables
+include 'sandbox.php';
+echo $test_var;
+
+?>
+
+<!------------- HTML ------------->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Patient Portal </title>
+  <link href='../css/style.css' rel='stylesheet'>
+  <script src="../js/effects.js"></script>
+  <!--- SOURCE: https://www.w3schools.com/php/php_includes.asp --->
+</head>
+
+<body>
+
+  <h2>Patient Portal</h2>
+
+</body>
+
+</html>
