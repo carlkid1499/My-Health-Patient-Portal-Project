a3e59844b2a58d9fd96c6077c9c417c2dc3c11aa
Carlos <carlos.santos112@outlook.com>
2020-10-25 15:29:14 -0700

    Patient Portal
    layoing out the bare bones for the patient portal page. Currently it display basic information about a patient.
    The log out button works. The others don't

--- php/patient_portal.php
+++ php/patient_portal.php
@@ -8,6 +8,13 @@
 include 'sandbox.php';
 echo $test_var;
 
+# Start the session again to access session variables
+session_start();
+# Grab all the session values
+$username = $_SESSION['username'];
+$userid = $_SESSION['userid'];
+$isemployee = $_SESSION['isemployee'];
+$pid = $_SESSION['pid'];
 ?>
 
 <!------------- HTML ------------->
@@ -23,8 +30,97 @@ echo $test_var;
 
 <body>
 
-  <h2>Patient Portal</h2>
+  <h2>Patient Portal: <?php echo " Welcome - $username"?></h2>
 
+  <section name="patientinfo">
+    <!-- This is the place where we get/print and patient informaton in the database. The patient must have access to only his/her info and no one else -->
+    <table name="patientinfo_table">
+      <!-- Populate table column names -->
+      <tr>
+        <th> Patient ID </th> 
+        <th> First name </th>
+        <th> Last name </th>
+        <th> Birthday </th>
+        <th> Gender </th>
+        <th> Address </th>
+        <th> Email </th>
+        <th> Phone </th>
+        <th> Emergency Contact Name </th>
+        <th> Emergency Phone Number </th>
+      </tr>
+      <!-- Populate table column values but let's get that data first! -->
+      <?php 
+
+        // Variables for Patient information
+        $name_fisrt = NULL;
+        $name_last = NULL;
+        $DOB = NULL;
+        $gender = NULL;
+        $address = NULL;
+        $email = NULL;
+        $phone = NULL;
+        $e_name = NULL;
+        $e_phone = NULL;
+
+        // Create connection for log in
+        $conn = new mysqli("localhost", "myhealth2", "CIOjh^J8h^?b", "myhealth2");
+        // Check if connection is valid
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+          $msg = "Connection failed: to DB";
+        }
+
+        // Query the PatientInfo database for the information
+        $results = $conn ->query("SELECT * FROM PatientInfo WHERE PID='$pid'");
+
+        // Did we get any results
+        if($results->num_rows >0)
+        {
+          // Get the Query Results
+          while ($row = $results->fetch_assoc()) {
+            $name_fisrt = $row["name_first"];
+            $name_last = $row["name_last"];
+            $DOB = $row["DOB"];
+            $gender = $row["Gender"];
+            $address = $row["address"];
+            $email = $row["email"];
+            $phone = $row["phone"];
+            $e_name = $row["Emergency_name"];
+            $e_phone = $row["Emergency_phone"];
+          } 
+        }
+        ?>
+      <tr>
+        <td><?php echo "$pid"?></td>
+        <td><?php echo "$name_fisrt"?></td>
+        <td><?php echo "$name_last"?></td>
+        <td><?php echo "$DOB"?></td>
+        <td><?php echo "$gender"?></td>
+        <td><?php echo "$address"?></td>
+        <td><?php echo "$email"?></td>
+        <td><?php echo "$phone"?></td>
+        <td><?php echo "$e_name"?></td>
+        <td><?php echo "$e_phone"?></td>
+      </tr>
+    </table>
+  </section>
+  <section name="options">
+    <!-- Let's put any actions the user (patient) can take. i.e update info, view records, etc -->
+    <form class="form-options" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
+                                                    ?>" method="post">
+      <button class="update-info-button" type="submit" name="update information">update information</button>
+      <button class="records-button" type="submit" name="records">view records</button> 
+      <button class="sign-out-button" type="submit" name="logout">logout
+        <!-- If the logout button is pushed -->
+        <?php if(isset($_POST['logout']))
+        {
+          header('Location: logout.php');
+        } 
+        ?>
+        </button>
+      </button>
+    </form>
+  </section>
+  <?php $conn->close(); ?>
 </body>
-
 </html>
