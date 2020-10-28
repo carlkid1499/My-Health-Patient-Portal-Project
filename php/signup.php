18c4144c3e2f4a5ef104e7e7a869beaac7f54c0c
Carlos <carlos.santos112@outlook.com>
2020-10-28 15:27:29 -0700

    signup.php
    signup php file works. A current patient should be able to sign up for an account using their PID, first and last name.

--- php/signup.php
+++ php/signup.php
@@ -6,6 +6,15 @@ to sign up for an account username and password.
 # Start the session
 ob_start();
 session_start();
+# Declare Global Vars for file
+$msg = "";
+// Create connection for log in
+$conn = new mysqli("localhost", "myhealth2", "CIOjh^J8h^?b", "myhealth2");
+// Check if connection is valid
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  $msg = "Connection failed: to DB";
+}
 
 ?>
 
@@ -24,8 +33,8 @@ session_start();
   <h2>My Health Patient Portal Signup Page</h2>
   <p> Please have your Patient ID ready! </p>
   <section class="signup_area" id="signup_area">
-  <form class="form-signup" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
-                                                    ?>" method="post">
+    <form class="form-signup" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
+                                                  ?>" method="post">
       <input type="text" class="form-signup" name="pid" placeholder="Patient ID" required></br></br>
       <input type="text" class="form-signup" name="name_first" placeholder="First Name" required></br></br>
       <input type="text" class="form-signup" name="name_last" placeholder="Last Name" required></br></br>
@@ -34,36 +43,50 @@ session_start();
       <input type="password" class="form-control" name="retype_password" placeholder="retype password" required>
       <button class="submit-button" type="submit" name="submit">submit
         <!-- If the submit button is pushed, we need to process the data. If successfull we need to redirect to the patient portal -->
-        <?php if(isset($_POST['submit']))
-          $msg = '';
-        {
-          // Process the form information, but first make sure nothing is empty
-          if(!empty($_POST['pid']) && !empty($_POST['name_first']) && !empty($_POST['name_last']) && !empty($_POST['password']) && !empty($_POST['retype_password']))
-          {
-            // Now we check if the password files match. Strip whitespace first
-            if(trim($_POST['password']) == trim($_POST['retype_password']))
-            {
-              // passwords match yay, lets process !
-              $pid = trim($_POST['pid']);
-              $name_first = trim($_POST['name_first']);
-              $name_last = trim($_POST['name_last']);
-              $password = trim($_POST['password']);
+        <?php if (isset($_POST['submit'])) {
+          // Process the form information
+          // Now we check if the password files match. Strip whitespace first
+          if (trim($_POST['password']) == trim($_POST['retype_password'])) {
+            // passwords match yay, lets process !
+            $pid = trim($_POST['pid']);
+            $name_first = trim($_POST['name_first']);
+            $name_last = trim($_POST['name_last']);
+            $password = trim($_POST['password']);
+            $username = trim($_POST['username']);
+            // We retrieve and update data based on the PID, first lets check if the PID is valid
+            // Query the PatientInfo database for the information
+            $results = $conn->query("SELECT PID FROM PatientInfo WHERE PID='$pid' AND name_first='$name_first' AND name_last='$name_last'");
 
-              // We retrieve and update data based in the PID, for now let's print it to the screen
-             $msg = "$pid, $name_first, $name_last, $password";
-            }
-            else
-            {
-              $msg = "password field did not match! Please try again!";
+            // Check if results is NULL, if it is the PID doesn't exist
+            if ($results == NULL) {
+              $msg = "Not a valid Patient ID, please contact tech support!";
+            } else {
+              // Create a random UserID. Max is unsigned int for mysql
+              $userid = rand(1, 4294967295);
+              // Check if it already exists in the table/DB.
+              $results = $conn->query("SELECT UserID FROM Users WHERE UserID='$userid'");
+              if ($results != NULL) {
+                // We need to insert into the Users table with the info provied
+                $results = $conn->query("INSERT INTO Users (UserID, PID, Username, UserPassword, IsEmployee) VALUES ('$userid','$pid', '$username', '$password', 0)");
+                if($results != NULL)
+                {
+                  // If we get here we inserted into users successfully
+                  header('Location: ../index.php');
+                }
+                else
+                {
+                  $msg = "error creating account please try again or contact support";
+                }
+              } else {
+                $msg = "We ran into an error please try again or contact support!";
+              }
             }
           }
-          else
-          {
-            $msg = "One or more fields are empty! Please try again!";
-          }
-        } 
+        } else {
+          $msg = "password field did not match! Please try again!";
+        }
         ?>
-        </button>
+      </button>
     </form>
   </section>
   <?php echo "$msg" ?>
