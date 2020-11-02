8e157358f927efe96a63c13900a23ccf3f246603
Carlos <carlos.santos112@outlook.com>
2020-11-01 20:47:31 -0800

    Fixes
    added in some parameters to fix the sql script.
    When referencing "varchar(255)" as a foreign key it must be a "unique" type as well. Otherwise we get a bunch of 1005 errors. I got this to work on my localhost environment.

--- Tables/createTables.sql
+++ Tables/createTables.sql
@@ -1,6 +1,6 @@
 # Create the Patient Information Table
-CREATE TABLE PatientInfo (
-  PID BIGINT  unsigned UNIQUE PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS PatientInfo (
+  PID BIGINT unsigned UNIQUE PRIMARY KEY NOT NULL,
   name_first VARCHAR(45) NOT NULL,
   name_last VARCHAR(45) NOT NULL,
   DOB DATE NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE PatientInfo (
 );
 
 # Create the Patient Notes table
-CREATE TABLE PatientNotes (
+CREATE TABLE IF NOT EXISTS PatientNotes (
 	# We can have multiple notes so we give each note a PNI (Patient Note ID)
 	PNI INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     PID BIGINT UNSIGNED NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE PatientNotes (
 );
 
 # Create the Patient Records table
-CREATE TABLE PatientRecords (
+CREATE TABLE IF NOT EXISTS PatientRecords (
 	# We can have multiple records so we give each a PRI (Patient Record ID)
 	PRI INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     PID BIGINT UNSIGNED NOT NULL,
@@ -40,71 +40,81 @@ CREATE TABLE PatientRecords (
     PatientPayment INT
 );
 
+#Create InsPlans table
+CREATE TABLE IF NOT EXISTS InsPlans(
+    PlanID INT UNSIGNED PRIMARY KEY,
+    Company VARCHAR(255) UNIQUE,
+    AnnualPrem BIGINT,
+    AnnualDeductible BIGINT,
+    AnnualCoverageLimit BIGINT,
+    LifetimeCoverage BIGINT,
+    Network VARCHAR(255) UNIQUE
+);
+
 # Create the Insurance Providers table
-CREATE TABLE IF NOT EXISTS InsProvier (
+CREATE TABLE IF NOT EXISTS InsProvider (
 	CompanyID INT UNSIGNED UNIQUE PRIMARY KEY NOT NULL,
-    Company VARCHAR(255),
+    Company VARCHAR(255) UNIQUE,
     # This will reference InsPlans PlanID eventually
     PlanID INT UNSIGNED,
+    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID),
     Category VARCHAR(255),
 	Address VARCHAR (255),
     Email VARCHAR (255),
     Phone VARCHAR (45)
 );
 
-#Create Enrolled table
-CREATE TABLE Enrolled(
-    PlanID INT UNSIGNED,
-    PID BIGINT UNSIGNED NOT NULL,
-    Company VARCHAR(255),
-    FOREIGN KEY(PlanID) REFERENCES InsPlans,
-    FOREIGN KEY(PID) REFERENCES PatientInfo,
-    FOREIGN KEY(Company) REFERENCES InsPlans
-);
-
-#Create InsPlans table
-CREATE TABLE InsPlans(
-    PlanID INT UNSIGNED,
-    Company VARCHAR(255),
-    AnnualPrem BIGINT,
-    AnnualDeductible BIGINT,
-    AnnualCoverageLimit BIGINT,
-    LifetimeCoverage BIGINT,
-    Network VARCHAR(255),
-    PRIMARY KEY(PlanID)
-);
-
 #Create Coverage table
-CREATE TABLE Coverage(
+CREATE TABLE IF NOT EXISTS Coverage(
     PlanID INT UNSIGNED,
-    Company VARCHAR(255),
-    TreatmentCategory VARCHAR(255),
-    FOREIGN KEY(PlanID) REFERENCES InsPlans,
-    FOREIGN KEY(Company) REFERENCES InsPlans
+    Company VARCHAR(255) UNIQUE,
+    TreatmentCategory VARCHAR(255) UNIQUE,
+    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID),
+    FOREIGN KEY(Company) REFERENCES InsPlans(Company)
 );
 
 #Create Membership table
-CREATE TABLE Membership(
+CREATE TABLE IF NOT EXISTS Membership(
     ProvID BIGINT UNSIGNED UNIQUE PRIMARY KEY NOT NULL,
     Network VARCHAR(255),
-    FOREIGN KEY(Network) REFERENCES InsPlans
+    FOREIGN KEY(Network) REFERENCES InsPlans(Network)
 );
 
 #Create InsCategories Table
-CREATE TABLE InsCategories(
+CREATE TABLE IF NOT EXISTS InsCategories(
     Company VARCHAR(255),
-    TreatmentCategory VARCHAR(255),
+    TreatmentCategory VARCHAR(255) UNIQUE,
     Subcategory VARCHAR(255),
-    FOREIGN KEY(TreatmentCategory) REFERENCES Coverage
+    FOREIGN KEY(TreatmentCategory) REFERENCES Coverage(TreatmentCategory)
 );
 
 #Create Costs Table
-CREATE TABLE Costs(
-    Company VARCHAR(255),
+CREATE TABLE IF NOT EXISTS Costs(
+    Company VARCHAR(255) UNIQUE,
     Treament VARCHAR(255),
     AllowedCost BIGINT,
     InNetworkCoverage BIGINT,
     OutNetworkCoverage BIGINT,
     FullDeductible BIGINT,
-    FOREIGN KEY(Company) REFERENCES InsPlans
+    FOREIGN KEY(Company) REFERENCES InsPlans(Company)
+);
+
+#Create Enrolled table
+CREATE TABLE IF NOT EXISTS Enrolled(
+    PlanID INT UNSIGNED,
+    PID BIGINT UNSIGNED,
+    Company VARCHAR(255) UNIQUE,
+    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID),
+    FOREIGN KEY(PID) REFERENCES PatientInfo(PID),
+    FOREIGN KEY(Company) REFERENCES InsPlans(Company)
+);
+
+# Create the Patient Information Table
+CREATE TABLE IF NOT EXISTS Users (
+	UserID INT UNSIGNED PRIMARY KEY UNIQUE NOT NULL,
+    PID BIGINT UNSIGNED UNIQUE,
+    FOREIGN KEY(PID) REFERENCES PatientInfo(PID),
+    UserName VARCHAR(25) UNIQUE,
+    UserPassword VARCHAR(25),
+    IsEmployee BOOL
 );
