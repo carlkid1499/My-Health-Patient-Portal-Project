6ae3128ba270d03d6b1a82eb96acd20c42fec069
Carlos <carlos.santos112@outlook.com>
2020-11-03 14:03:19 -0800

    Reorg
    changed the way the script runs. I run into issues this way by splitting up things.

--- Tables/createTables.sql
+++ Tables/createTables.sql
@@ -1,3 +1,4 @@
+# Step 1. Create all the tables first
 # Create the Patient Information Table
 CREATE TABLE IF NOT EXISTS PatientInfo (
   PID BIGINT unsigned UNIQUE PRIMARY KEY NOT NULL,
@@ -17,7 +18,6 @@ CREATE TABLE IF NOT EXISTS PatientNotes (
 	# We can have multiple notes so we give each note a PNI (Patient Note ID)
 	PNI INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     PID BIGINT UNSIGNED NOT NULL,
-    FOREIGN KEY (PID) REFERENCES PatientInfo(PID),
     ProvID INT NOT NULL,
     NoteTime DATE,
     DiagnosisNotes VARCHAR(255),
@@ -30,7 +30,6 @@ CREATE TABLE IF NOT EXISTS PatientRecords (
 	# We can have multiple records so we give each a PRI (Patient Record ID)
 	PRI INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     PID BIGINT UNSIGNED NOT NULL,
-    FOREIGN KEY (PID) REFERENCES PatientInfo(PID),
 	# PROV ID GOES HERE its an FK
 	RecordTime DATE,
     Treatment VARCHAR(255),
@@ -44,7 +43,6 @@ CREATE TABLE IF NOT EXISTS PatientRecords (
 CREATE TABLE IF NOT EXISTS InsPlans(
     PlanID INT UNSIGNED PRIMARY KEY,
     CompanyID INT UNSIGNED UNIQUE DEFAULT NULL,
-    FOREIGN KEY(CompanyID) REFERENCES InsProvider(CompanyID),
     AnnualPrem BIGINT,
     AnnualDeductible BIGINT,
     AnnualCoverageLimit BIGINT,
@@ -58,7 +56,6 @@ CREATE TABLE IF NOT EXISTS InsProvider (
     Company VARCHAR(255),
     # This will reference InsPlans PlanID eventually
     PlanID INT UNSIGNED,
-    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID),
     Category VARCHAR(255),
 	Address VARCHAR (255),
     Email VARCHAR (255),
@@ -69,31 +66,25 @@ CREATE TABLE IF NOT EXISTS InsProvider (
 CREATE TABLE IF NOT EXISTS Coverage(
     PlanID INT UNSIGNED,
     CompanyID INT UNSIGNED,
-    FOREIGN KEY(CompanyID) REFERENCES InsProvider(CompanyID),
-    TreatmentCategory VARCHAR(255) UNIQUE,
-    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID)
+    TreatmentCategory VARCHAR(255) UNIQUE
 );
 
 #Create Membership table
 CREATE TABLE IF NOT EXISTS Membership(
     ProvID BIGINT UNSIGNED UNIQUE PRIMARY KEY NOT NULL,
-    Network VARCHAR(255),
-    FOREIGN KEY(Network) REFERENCES InsPlans(Network)
+    Network VARCHAR(255)
 );
 
 #Create InsCategories Table
 CREATE TABLE IF NOT EXISTS InsCategories(
     CompanyID INT UNSIGNED,
-    FOREIGN KEY(CompanyID) REFERENCES InsProvider(CompanyID),
     TreatmentCategory VARCHAR(255) UNIQUE,
-    Subcategory VARCHAR(255),
-    FOREIGN KEY(TreatmentCategory) REFERENCES Coverage(TreatmentCategory)
+    Subcategory VARCHAR(255)
 );
 
 #Create Costs Table
 CREATE TABLE IF NOT EXISTS Costs(
     CompanyID INT UNSIGNED,
-    FOREIGN KEY(CompanyID) REFERENCES InsProvider(CompanyID),
     Treament VARCHAR(255),
     AllowedCost BIGINT,
     InNetworkCoverage BIGINT,
@@ -105,18 +96,50 @@ CREATE TABLE IF NOT EXISTS Costs(
 CREATE TABLE IF NOT EXISTS Enrolled(
     PlanID INT UNSIGNED,
     PID BIGINT UNSIGNED,
-    CompanyID INT UNSIGNED,
-    FOREIGN KEY(CompanyID) REFERENCES InsProvider(CompanyID),
-    FOREIGN KEY(PlanID) REFERENCES InsPlans(PlanID),
-    FOREIGN KEY(PID) REFERENCES PatientInfo(PID)
+    CompanyID INT UNSIGNED
 );
 
 # Create the Patient Information Table
 CREATE TABLE IF NOT EXISTS Users (
 	UserID INT UNSIGNED PRIMARY KEY UNIQUE NOT NULL,
     PID BIGINT UNSIGNED UNIQUE,
-    FOREIGN KEY(PID) REFERENCES PatientInfo(PID),
     UserName VARCHAR(25) UNIQUE,
     UserPassword VARCHAR(25),
     IsEmployee BOOL
 );
+
+# Step 2 Add in constraints and such
+ALTER TABLE Costs
+  ADD CONSTRAINT FOREIGN KEY (CompanyID) REFERENCES InsProvider (CompanyID);
+
+ALTER TABLE Coverage
+  ADD CONSTRAINT FOREIGN KEY (CompanyID) REFERENCES InsProvider (CompanyID),
+  ADD CONSTRAINT FOREIGN KEY (PlanID) REFERENCES InsPlans (PlanID);
+
+ALTER TABLE Enrolled
+  ADD CONSTRAINT FOREIGN KEY (CompanyID) REFERENCES InsProvider (CompanyID),
+  ADD CONSTRAINT FOREIGN KEY (PlanID) REFERENCES InsPlans (PlanID),
+  ADD CONSTRAINT FOREIGN KEY (PID) REFERENCES PatientInfo (PID);
+
+ALTER TABLE InsCategories
+  ADD CONSTRAINT FOREIGN KEY (CompanyID) REFERENCES InsProvider (CompanyID),
+  ADD CONSTRAINT FOREIGN KEY (TreatmentCategory) REFERENCES Coverage (TreatmentCategory);
+
+ALTER TABLE InsProvider
+  ADD CONSTRAINT FOREIGN KEY (PlanID) REFERENCES InsPlans (PlanID),
+  ADD CONSTRAINT FOREIGN KEY (CompanyID) REFERENCES InsPlans (CompanyID);
+
+ALTER TABLE Membership
+  ADD CONSTRAINT FOREIGN KEY (Network) REFERENCES Insplans (Network);
+
+ALTER TABLE PatientNotes
+  ADD CONSTRAINT FOREIGN KEY (PID) REFERENCES PatientInfo (PID);
+
+ALTER TABLE PatientRecords
+  ADD CONSTRAINT FOREIGN KEY (PID) REFERENCES PatientInfo (PID);
+
+ALTER TABLE Users
+  ADD CONSTRAINT FOREIGN KEY (PID) REFERENCES PatientInfo (PID);
+ 
+ #  Step 3. Save your changes
+COMMIT;
