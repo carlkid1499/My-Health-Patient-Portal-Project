710a2dc00ba1a3d3e0fc94ac4b7e4dd473b83e6a
carlkid1499 <carlos.santos112@outlook.com>
2020-12-11 15:15:48 -0800

    FIX IT!
    found the logical error! for an if statement branch

--- php/insurance_plans.php
+++ php/insurance_plans.php
@@ -195,154 +195,151 @@ $get_enrolled_query->bind_result($planid, $companyid);
               echo "</table>";
               $get_planid_info_by_id_query->close();
             }
-          } else {
-            echo "<B> You are not enrolled! Select a state to begin! 
-            Then select an Insurance Provider Plan for more information!
-            <B>";
-            echo "
-            <div class=\"container\">
-          <section class=\"insurance_plans\" id=\"insurance_plans\">
-          <form action=\"\" method=\"post\">
-            <div class=\"center\">
-              <p> State </p>
-              <select name=\"State\">
-                <option value=\"DEFAULT\"> State Abbr.</option>
-                <option value=\"AL\"> Alabama - AL </option>
-                <option value=\"AK\"> Alaska - AK </option>
-                <option value=\"AZ\"> Arizona - AZ </option>
-                <option value=\"AR\"> Arkansas - AR </option>
-                <option value=\"CA\"> California - CA </option>
-                <option value=\"Co\"> Colorado - CO </option>
-                <option value=\"CT\"> Connecticut - CT </option>
-                <option value=\"DE\"> Delaware - DE </option>
-                <option value=\"FL\"> Florida - FL </option>
-                <option value=\"GA\"> Georgia - GA </option>
-                <option value=\"HI\"> Hawaii - HI </option>
-                <option value=\"ID\"> Idaho - ID </option>
-                <option value=\"IL\"> Illinois - IL </option>
-                <option value=\"IN\"> Indiana - IN </option>
-                <option value=\"IA\"> Iowa - IA </option>
-                <option value=\"KS\"> Kansas - KS </option>
-                <option value=\"KY\"> Kentucky - KY </option>
-                <option value=\"LA\"> Louisiana - LA </option>
-                <option value=\"ME\"> Maine - ME </option>
-                <option value=\"MD\"> Maryland - MD </option>
-                <option value=\"MA\"> Massachusetts - MA </option>
-                <option value=\"MI\"> Michigan - MI </option>
-                <option value=\"MN\"> Minnesota - MN </option>
-                <option value=\"MS\"> Mississippi - MS </option>
-                <option value=\"MO\"> Missouri - MO </option>
-                <option value=\"MT\"> Montana - MT </option>
-                <option value=\"NE\"> Nebraska - NE </option>
-                <option value=\"NV\"> Nevada - NV </option>
-                <option value=\"NH\"> New Hampshire - NH </option>
-                <option value=\"NJ\"> New Jersey - NJ </option>
-                <option value=\"NM\"> New Mexico - NM </option>
-                <option value=\"NY\"> New York - NY </option>
-                <option value=\"NC\"> North Carolina - NC </option>
-                <option value=\"ND\"> North Dakota - ND </option>
-                <option value=\"OH\"> Ohio - OH </option>
-                <option value=\"OK\"> Oklahoma - OK </option>
-                <option value=\"OR\"> Oregon - OR </option>
-                <option value=\"PA\"> Pennsylvania - PA </option>
-                <option value=\"RI\"> Rhode Island - RI </option>
-                <option value=\"SC\"> South Carolina - SC </option>
-                <option value=\"SD\"> South Dakota - SD </option>
-                <option value=\"TN\"> Tennessee - TN </option>
-                <option value=\"TX\"> Texas - TX </option>
-                <option value=\"UT\"> Utah - UT </option>
-                <option value=\"VT\"> Vermont - VT </option>
-                <option value=\"VA\"> Virginia - VA </option>
-                <option value=\"WA\"> Washington - WA </option>
-                <option value=\"WV\"> West Virginia - WV </option>
-                <option value=\"WI\"> Wisconsin - WI </option>
-                <option value=\"WY\"> Wyoming - WY </option>
-              </select>
-              <input class=\"w3-bar-item w3-button logoutbtn\" type=\"submit\" name=\"button\" value=\"Submit\"/>
-
-              </div>
-            <div class=\"center\">
-          </form>      
-            ";
-
-            // Get the drop down data
-            if (isset($_POST['State']))
-              //  Check To make sure it's not the DEFAULT
-              if ($_POST['State'] == "DEFAULT")
-                echo "Please select a state!  Try again!";
-              else {
-                //  We take the state and query for it in the Insurance Provider table
-                $state = $_POST['State'];
-                $state_string = "%, $state%";
-                $search_for_insprov_by_state_query->bind_param("s", $state_string);
-                $search_for_insprov_by_state_query->execute();
-
-                # Decalre the variables to store the results in
-                $companyid = null;
-                $company = null;
-                $planid = null;
-                $category = null;
-                $address = null;
-                $email = null;
-                $phone = null;
-
-                if ($search_for_insprov_by_state_query->bind_result(
-                  $companyid,
-                  $company,
-                  $planid,
-                  $category,
-                  $address,
-                  $email,
-                  $phone
-                )) {
-
-                  # Create the Ins Provider Table
-                  echo "
-                  <center>
-                  <table name=\"insplans_table\" class=\"center\" style=\"width=95%;\" border=\"3\" cellpadding=\"1\">
-                  <tr>
-                    <th> CheckBox </th>
-                    <th> CompanyID </th>
-                    <th> Company Name</th>
-                    <th> PlanID </th>
-                    <th> Category </th>
-                    <th> Address </th>
-                    <th> Phone </th>
-                    <th> Email </th>
-                  </tr>
-                  </center>";
-
-                  // get the resuts
-                  $check_box_id = 0;
-                  while ($search_for_insprov_by_state_query->fetch()) {
-                    # Print each table row
-                    echo "<tr>
-                    <td><input type=\"checkbox\" id=\"$check_box_id\" /></td>
-                    <td>$companyid</td>
-                    <td>$company</td>
-                    <td>$planid</td>
-                    <td>$category</td>
-                    <td>$address</td>
-                    <td>$phone</td>
-                    <td>$email</td>
-                    </tr>";
-                    // increment the id
-                    $check_box_id++;
-                  }
-
-                  // Close the Ins Provider Table and the query
-                  echo "</table>";
-                  $search_for_insprov_by_state_query->close();
-                }
-              }
           }
         }
+        else {
+          echo "<B> You are not enrolled! Select a state to begin! 
+          Then select an Insurance Provider Plan for more information!
+          <B>";
+          echo "
+          <div class=\"container\">
+        <section class=\"insurance_plans\" id=\"insurance_plans\">
+        <form action=\"\" method=\"post\">
+          <div class=\"center\">
+            <p> State </p>
+            <select name=\"State\">
+              <option value=\"DEFAULT\"> State Abbr.</option>
+              <option value=\"AL\"> Alabama - AL </option>
+              <option value=\"AK\"> Alaska - AK </option>
+              <option value=\"AZ\"> Arizona - AZ </option>
+              <option value=\"AR\"> Arkansas - AR </option>
+              <option value=\"CA\"> California - CA </option>
+              <option value=\"Co\"> Colorado - CO </option>
+              <option value=\"CT\"> Connecticut - CT </option>
+              <option value=\"DE\"> Delaware - DE </option>
+              <option value=\"FL\"> Florida - FL </option>
+              <option value=\"GA\"> Georgia - GA </option>
+              <option value=\"HI\"> Hawaii - HI </option>
+              <option value=\"ID\"> Idaho - ID </option>
+              <option value=\"IL\"> Illinois - IL </option>
+              <option value=\"IN\"> Indiana - IN </option>
+              <option value=\"IA\"> Iowa - IA </option>
+              <option value=\"KS\"> Kansas - KS </option>
+              <option value=\"KY\"> Kentucky - KY </option>
+              <option value=\"LA\"> Louisiana - LA </option>
+              <option value=\"ME\"> Maine - ME </option>
+              <option value=\"MD\"> Maryland - MD </option>
+              <option value=\"MA\"> Massachusetts - MA </option>
+              <option value=\"MI\"> Michigan - MI </option>
+              <option value=\"MN\"> Minnesota - MN </option>
+              <option value=\"MS\"> Mississippi - MS </option>
+              <option value=\"MO\"> Missouri - MO </option>
+              <option value=\"MT\"> Montana - MT </option>
+              <option value=\"NE\"> Nebraska - NE </option>
+              <option value=\"NV\"> Nevada - NV </option>
+              <option value=\"NH\"> New Hampshire - NH </option>
+              <option value=\"NJ\"> New Jersey - NJ </option>
+              <option value=\"NM\"> New Mexico - NM </option>
+              <option value=\"NY\"> New York - NY </option>
+              <option value=\"NC\"> North Carolina - NC </option>
+              <option value=\"ND\"> North Dakota - ND </option>
+              <option value=\"OH\"> Ohio - OH </option>
+              <option value=\"OK\"> Oklahoma - OK </option>
+              <option value=\"OR\"> Oregon - OR </option>
+              <option value=\"PA\"> Pennsylvania - PA </option>
+              <option value=\"RI\"> Rhode Island - RI </option>
+              <option value=\"SC\"> South Carolina - SC </option>
+              <option value=\"SD\"> South Dakota - SD </option>
+              <option value=\"TN\"> Tennessee - TN </option>
+              <option value=\"TX\"> Texas - TX </option>
+              <option value=\"UT\"> Utah - UT </option>
+              <option value=\"VT\"> Vermont - VT </option>
+              <option value=\"VA\"> Virginia - VA </option>
+              <option value=\"WA\"> Washington - WA </option>
+              <option value=\"WV\"> West Virginia - WV </option>
+              <option value=\"WI\"> Wisconsin - WI </option>
+              <option value=\"WY\"> Wyoming - WY </option>
+            </select>
+            <input class=\"w3-bar-item w3-button logoutbtn\" type=\"submit\" name=\"button\" value=\"Submit\"/>
 
+            </div>
+          <div class=\"center\">
+        </form>      
+          ";
+
+          // Get the drop down data
+          if (isset($_POST['State']))
+            //  Check To make sure it's not the DEFAULT
+            if ($_POST['State'] == "DEFAULT")
+              echo "Please select a state!  Try again!";
+            else {
+              //  We take the state and query for it in the Insurance Provider table
+              $state = $_POST['State'];
+              $state_string = "%, $state%";
+              $search_for_insprov_by_state_query->bind_param("s", $state_string);
+              $search_for_insprov_by_state_query->execute();
+
+              # Decalre the variables to store the results in
+              $companyid = null;
+              $company = null;
+              $planid = null;
+              $category = null;
+              $address = null;
+              $email = null;
+              $phone = null;
+
+              if ($search_for_insprov_by_state_query->bind_result(
+                $companyid,
+                $company,
+                $planid,
+                $category,
+                $address,
+                $email,
+                $phone
+              )) {
+
+                # Create the Ins Provider Table
+                echo "
+                <center>
+                <table name=\"insplans_table\" class=\"center\" style=\"width=95%;\" border=\"3\" cellpadding=\"1\">
+                <tr>
+                  <th> CheckBox </th>
+                  <th> CompanyID </th>
+                  <th> Company Name</th>
+                  <th> PlanID </th>
+                  <th> Category </th>
+                  <th> Address </th>
+                  <th> Phone </th>
+                  <th> Email </th>
+                </tr>
+                </center>";
+
+                // get the resuts
+                $check_box_id = 0;
+                while ($search_for_insprov_by_state_query->fetch()) {
+                  # Print each table row
+                  echo "<tr>
+                  <td><input type=\"checkbox\" id=\"$check_box_id\" /></td>
+                  <td>$companyid</td>
+                  <td>$company</td>
+                  <td>$planid</td>
+                  <td>$category</td>
+                  <td>$address</td>
+                  <td>$phone</td>
+                  <td>$email</td>
+                  </tr>";
+                  // increment the id
+                  $check_box_id++;
+                }
+
+                // Close the Ins Provider Table and the query
+                echo "</table>";
+                $search_for_insprov_by_state_query->close();
+              }
+            }
+        }
         // Close the query we are done with it
         $get_enrolled_query->close();
-
-
-
         ?>
       </section>
 
