e8302c308b86dcaf273c65ea8d7d499d2f88a4cb
Keller <55157917+keeperkell@users.noreply.github.com>
2020-12-11 15:11:22 -0800

    created pharmacy portal, added redirect on worker login

--- php/pharmacy_portal.php
+++ php/pharmacy_portal.php
@@ -15,11 +15,6 @@ $username = $_SESSION['username'];
 $userid = $_SESSION['userid'];
 $isemployee = $_SESSION['isemployee'];
 $pid = $_SESSION['pid'];
-
-if($username == "pharmacy" || $username == "Pharmacy"){
-  header('Location: pharmacy_portal.php');
-}
-
 ?>
 
 <!------------- HTML ------------->
@@ -77,7 +72,7 @@ if($username == "pharmacy" || $username == "Pharmacy"){
 </div>
 
 <div class="header w3-theme-d2">  
-  <h1><b>My Health Patient Portal</b></h1>
+  <h1><b>My Health Pharmacy </b></h1>
 </div>
 
 <body>
@@ -87,104 +82,13 @@ if($username == "pharmacy" || $username == "Pharmacy"){
   <div class="container">
   <section class="seachbar-section">
     <form class="searchbar" id="searchbard" action="healthcare_worker_portal.php" method="post" style="margin:auto; max-width=75%">
-      <input type="text" placeholder="Patient Search Criteria" name="searchbar-text">
+      <input type="text" placeholder="Patient Search Criteria: Last name, First name, DOB" name="searchbar-text">
     </form>
   </div>
   <div>
-    <div class="container">
-    <select name="search_by_options_list" class="align-middle">
-      <option name="search_by_options" id="search_by_options">Search by:</option>
-      <option name="search_by_options" id="search_by_options">Search by: First Name, Last Name, DOB</option>
-      <option name="search_by_options" id="search_by_options">Search by: Patient ID </option>
-      <option name="search_by_options" id="search_by_options">Search by: Patient Phone Number </option>
-    </select>
-      </div>
     <button class="searchbtn" id="searchbar-button" value="searchbar-button" onclick="return checkInput();">search</button>
   </div>
   </section>
-</div>
-<div class>
-  <form action="healthcare_worker_portal.php" method="post" id="options">
-
-    <!-- QUERY OPTIONS SECTION -->
-
-    <section class="block-of-text">
-      <fieldset>
-        <legend>Target Database</legend>
-
-        <!-- populate drop-down list -->
-        <?php
-
-        $list = '<option name = "sqldblist">Select Database</option>';
-
-        //Extract list of all databases
-        $q = 'show databases;';
-        if ($dblist = mysqli_query($conn, $q)) {
-          while ($row = mysqli_fetch_array($dblist)) {
-            $val = $row['Database'];
-            if (array_search($val, $defaultTables) === false) # exclude defauls mysql tables
-            {
-              $list .= '<option';
-              $list .= $val == $targetDB ? ' selected = \'selected\'>' : '>';
-              $list .= $val . '</option>';
-            }
-          }
-          mysqli_free_result($dblist);
-        }
-        ?>
-
-        <select name="sqldblist">
-          <?php echo $list; ?>
-        </select>
-
-        <br>
-
-      </fieldset>
-
-    </section>
-
-    <!-- INPUT SECTION -->
-    <section class="block-of-text">
-      <fieldset>
-        <legend>Input</legend>
-
-        <textarea class="FormElement" name="inputQuery" id="input" cols="40" rows="10" placeholder="Type Query Here"><?php echo $inputQuery; ?></textarea>
-
-        <br>
-
-        <input type="submit" id="submit" name="submit" value="Submit" onclick="return checkInput();">
-
-      </fieldset>
-    </section>
-  </form>
-
-  <!-- OUTPUT SECTION -->
-  <form action="healthcare_worker_portal.php" method="post">
-
-    <section class="block-of-text">
-      <fieldset>
-        <legend>Output</legend>
-
-        <?php
-
-
-        if ($search_result != NULL) {
-          //output data of each row
-
-          if ($search_result->num_rows > 0) {
-            while ($row = $search_result->fetch_assoc()) {
-              echo "PID: " . $row["PID"] . " First Name: " . $row["name_first"] . " Last Name: " . $row["name_last"] . " DOB: " . $row["DOB"] . "<br>";
-            }
-          } else {
-            echo "0 results!";
-          }
-        }
-        ?>
-
-
-      </fieldset>
-    </section>
-  </form>
 </div>
 
   <?php $conn->close(); ?>
